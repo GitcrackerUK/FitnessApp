8dd7905c1383c1782a6c248b5c78979036e2102e
gitcrackerUK <pfsiwek@gmail.com>
2020-10-08 15:24:41 +0100

    Updates

---
+++ precache-manifest.893edda57ed6c5d076e87bfc99a7e9dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc039daa5e0de8e6f4c301df2ab12dbb",
+    "url": "/FitnessApp/index.html"
+  },
+  {
+    "revision": "a1d0c8110c2d4e3a0360",
+    "url": "/FitnessApp/static/css/2.ce595351.chunk.css"
+  },
+  {
+    "revision": "a1d0c8110c2d4e3a0360",
+    "url": "/FitnessApp/static/js/2.8db78d61.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/FitnessApp/static/js/2.8db78d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e847a225272d33531b5a",
+    "url": "/FitnessApp/static/js/main.5e256636.chunk.js"
+  },
+  {
+    "revision": "6049608942d096bbed30",
+    "url": "/FitnessApp/static/js/runtime-main.e9bca376.js"
+  }
+]);
