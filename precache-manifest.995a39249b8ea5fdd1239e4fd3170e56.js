f9316ae5bb93ab57c7aa80ccc1185e38d2d5074c
gitcrackerUK <pfsiwek@gmail.com>
2020-10-08 14:49:17 +0100

    initial git init

---
+++ precache-manifest.995a39249b8ea5fdd1239e4fd3170e56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c99041387ca52b40f2146ca72bb225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1d0c8110c2d4e3a0360",
+    "url": "/static/css/2.ce595351.chunk.css"
+  },
+  {
+    "revision": "a1d0c8110c2d4e3a0360",
+    "url": "/static/js/2.8db78d61.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.8db78d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b0d07cb291a6c6c919",
+    "url": "/static/js/main.b8a2abfd.chunk.js"
+  },
+  {
+    "revision": "fbe12edccac0ea1a736e",
+    "url": "/static/js/runtime-main.393320e5.js"
+  }
+]);
